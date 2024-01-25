c769dbd43dd19f8d7a3dd7c825b3f9dc48af3f3c
Sy Roman <osiris.roman@protonmail.com>
2024-01-25 13:04:35 -0600

    updated config

--- variables.tf
+++ variables.tf
@@ -24,7 +24,25 @@ variable "imgversion" {}
 variable "disk_caching" {}
 variable "storage_account_type" {}
 
-
-
-
-
+variable "forList" {
+  type = map(object({
+    name      = string
+    age       = number
+    eyecolor  = string
+    haircolor = string
+  }))
+  default = {
+    "david" = {
+      name      = "david"
+      age       = 30
+      eyecolor  = "brown"
+      haircolor = "brown"
+    }
+    "stan" = {
+      name      = "stan"
+      age       = 50
+      eyecolor  = "blue"
+      haircolor = "blond"
+    }
+  }
+}
